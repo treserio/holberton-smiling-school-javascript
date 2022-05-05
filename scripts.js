7e51f3c958d7b8dc2e18aaecfe8b5391e7883326
Tres Serio <treserio@hotmail.com>
2022-05-04 20:34:12 -0500

    +comments dynamicly load, most-pop 2 & 1 finished

--- scripts.js
+++ scripts.js
@@ -1,18 +1,45 @@
+// load comments from api
+$.get('https://smileschool-api.hbtn.info/quotes', (data) => {
+  console.log('cmntData', data);
+  const cmntList = [];
+  for (let item of data) {
+    cmntList.push(createCmnt(item));
+  }
+  console.log(cmntList);
+  oneStepCaro_nItems(cmntList, 1, $('.comments .carousel-inner')[0]);
+})
+  .done(() => { $('.comments .loader').hide(); });
+
+// load most popular videos from api
 $.get('https://smileschool-api.hbtn.info/popular-tutorials', (data) => {
   // console.log('data', data);
   const cardList = [];
-  for (item of data) {
+  for (let item of data) {
     cardList.push(createCard(item));
   }
-  console.log(data);
+  console.log(cardList);
   oneStepCaro_nItems(cardList, 4, $('.pop-vids-4 .carousel-inner')[0]);
+  oneStepCaro_nItems(cardList, 2, $('.pop-vids-2 .carousel-inner')[0]);
+  oneStepCaro_nItems(cardList, 1, $('.pop-vids-1 .carousel-inner')[0]);
 })
-  .done(() => { $('.loader').hide(); });
+  .done(() => { $('.most-pop .loader').hide(); });
+
+function createCmnt(info) {
+  const cmnt = $('<div class="d-flex flex-column flex-md-row justify-content-around justify-content-md-center align-items-center">')[0];
+  let cmntContent = `<img class="img-fluid rounded-circle mb-4 mb-md-0" src="${info.pic_url}" alt="profile_5" width="160px" height="160px">
+      <div class="comment-text ml-md-5 mr-md-0 flex-column">
+        <div>« ${info.text}</div>
+        <h4 class="mt-3 mb-0">${info.name}</h4>
+        <i>${info.title}</i>
+      </div>`;
+  $(cmnt).append(cmntContent);
+  return cmnt;
+}
 
 function createCard(info) {
   const card = document.createElement('div');
   $(card).addClass('card border-0');
-  cardContent = `<div class="card-header">
+  let cardContent = `<div class="card-header">
       <img src="${info.thumb_url}" width="255" height="154">
       <img class="play-btn" src="images/play.png" width="64" height="64">
     </div>
@@ -40,17 +67,21 @@ function createCard(info) {
 }
 
 function oneStepCaro_nItems(cardList, nItems, target) {
-  console.log('cardList', cardList);
-  console.log('nItems', nItems);
-  console.log('target', target);
+  // console.log('cardList', cardList);
+  // console.log('nItems', nItems);
+  // console.log('target', target);
   for (let i = 0; i < cardList.length; ++i) {
     var carouselItem = $('<div class=carousel-item></div>')[0];
-    var flexSetup = $('<div class="d-flex flex-row justify-content-around"></div>')[0];
+    // check for the correct type of object being added for parent settings
+    if ($(cardList[0]).hasClass('card')) {
+      var flexSetup = $('<div class="d-flex flex-row justify-content-around"></div>')[0];
+    } else {
+      var flexSetup = document.createDocumentFragment();
+    }
     if (!i) {
       $(carouselItem).addClass('active');
     }
     for (let j = i; j < i + nItems; ++j) {
-      console.log(cardList[j % cardList.length]);
       flexSetup.append(cardList[j % cardList.length]);
     }
     carouselItem.append(flexSetup.cloneNode(true));
